68c69e29a6a22b871793b5409907faf24a8627a1
jessie <fxiaohong@sina.cn>
2024-03-19 14:34:57 +0800

    Update backup.sh

--- postgresql-backup/backup.sh
+++ postgresql-backup/backup.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-#gzip 压缩备份导出，在恢复备份时，需要使用 `gunzip` 或者类似工具解压备份文件
+echo "开始备份..."
 pg_dump -U ${POSTGRES_USER} -d ${POSTGRES_DB} -h ${POSTGRES_HOST} -p ${POSTGRES_PORT} | gzip > /backups/${POSTGRES_DB}-$(date +%Y-%m-%d-%H-%M-%S).sql.gz
+echo "备份完成..."
 
-#删除最新的20个文件之外的所有文件
+echo "开始清除历史数据..."
 ls -1tr /backups | head -n -20 | xargs -d '\n' rm -f --
+echo "除历史数据完成..."
